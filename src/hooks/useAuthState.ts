c05ceab6b69f5f1f3905f524af34590b83a9c6e0
u1aryz <u1aryz.d@gmail.com>
2023-05-20 14:43:08 +0900

    認証ステータスのhooksを追加

---
+++ src/hooks/useAuthState.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../supabase.ts";
+import { Session } from "@supabase/supabase-js";
+
+export const useAuthState = () => {
+	const [session, setSession] = useState<Session | null>();
+
+	useEffect(() => {
+		const { data: authListener } = supabase.auth.onAuthStateChange(
+			(_event, session) => {
+				setSession(session);
+			},
+		);
+		return () => {
+			authListener.subscription.unsubscribe();
+		};
+	}, []);
+
+	return session;
+};
