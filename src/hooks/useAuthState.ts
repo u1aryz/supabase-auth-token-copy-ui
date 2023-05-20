959602cfc1ccb6746ca2dfc31ed96027af239f31
u1aryz <u1aryz.d@gmail.com>
2023-05-20 14:47:02 +0900

    import aliasを追加

--- src/hooks/useAuthState.ts
+++ src/hooks/useAuthState.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../supabase.ts";
+import { supabase } from "@/supabase.ts";
 import { Session } from "@supabase/supabase-js";
 
 export const useAuthState = () => {
